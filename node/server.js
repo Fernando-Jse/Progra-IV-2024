4031fcbc8f0731998b6bb3bdf62c31e34b429674
Fernando-Jse <ferloto1304@gmail.com>
2024-05-13 10:22:35 -0600

    Se agrego la lista de usuarios y modificar

--- node/server.js
+++ node/server.js
@@ -2,13 +2,13 @@ const express = require('express'),
     app = express(),
     http = require('http').Server(app),
     io = require('socket.io')(http,{
-        allowEI03:true,
+        allowEIO3:true,
         cors:{
             origin:['http://localhost'],
             credential:true
         }
     }),
-    { MongoClient } = require('mongodb'),
+    { MongoClient, ObjectId } = require('mongodb'),
     url = 'mongodb://127.0.0.1:27017',
     client = new MongoClient(url),
     dbname = 'chatUGB',
@@ -22,11 +22,23 @@ async function conectarMongoDb(){
 app.get('/', (req, resp)=>{
     resp.sendFile(__dirname + '/index.html');
 });
-app.post('/usuarios/guardar',async (req, resp)=>{
+app.get('/usuarios/listar',async(req, resp)=>{
     let db = await conectarMongoDb(),
-        collection = db.collection('usuarios');
-    collection.insertOne(req.body);
-    resp.send(req.body);
+        collection=db.collection('usuarios');
+    let result = await collection.find({}).toArray();
+    resp.send(result);
+});
+app.post('/usuarios/guardar',async(req, resp)=>{
+    let db = await conectarMongoDb(),
+        collection=db.collection('usuarios'),
+        result = {};
+    if( req.body.accion == 'nuevo' ){
+        result = await collection.insertOne(req.body.usuario);
+    }else if( req.body.accion == 'modificar' ){
+        let idUsuario = new ObjectId(req.body.idUsuario);
+        result = await collection.updateOne({ _id: idUsuario }, { $set: req.body.usuario });
+    }
+    resp.send(result);
 });
 http.listen(port, event=>{
     console.log("Server ejecutado en el puerto", port);
